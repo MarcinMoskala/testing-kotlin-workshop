b7210cc0bc76e5e8f888e0ccb2473f65b6d297e2
Marcin Moskała <marcinmoskala@gmail.com>
2024-06-06 09:51:47 +0200

    Update

--- src/main/kotlin/ObserveAppointmentsUseCase.kt
+++ src/main/kotlin/ObserveAppointmentsUseCase.kt
@@ -9,26 +9,26 @@ import kotlinx.coroutines.flow.retry
 import java.time.Instant
 
 class ObserveAppointmentsUseCase(
-    private val appointmentsRepository: AppointmentsRepository
+    private val appointmentRepository: AppointmentRepository
 ) {
     // should observe only updates
     // should ignore repeating values
     // should retry API exception
     // should not retry API exception with incorrect code
     // should not retry non-API exceptions
-    fun observeAppointments(): Flow<List<Appointment>> = appointmentsRepository
+    fun observeAppointments(): Flow<List<Appointment>> = appointmentRepository
         .observeAppointments()
-        .filterIsInstance<AppointmentsUpdate>()
+        .filterIsInstance<AppointmentUpdate>()
         .map { it.appointments }
         .distinctUntilChanged()
         .retry { it is ApiException && it.code in 500..599 }
 }
 
-interface AppointmentsRepository {
-    fun observeAppointments(): Flow<AppointmentsEvent>
+interface AppointmentRepository {
+    fun observeAppointments(): Flow<AppointmentEvent>
 }
 
-sealed class AppointmentsEvent
-data class AppointmentsUpdate(val appointments: List<Appointment>): AppointmentsEvent()
-object AppointmentsConfirmed: AppointmentsEvent()
+sealed class AppointmentEvent
+data class AppointmentUpdate(val appointments: List<Appointment>): AppointmentEvent()
+object AppointmentConfirmed: AppointmentEvent()
 data class Appointment(val title: String, val time: Instant)
